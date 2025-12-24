0d4109cb0e1b8d68ae9f071e39fe6159e66ba1f3
Mohamed Rifays <rifaysmohamed2005@gmail.com>
2025-12-24 21:46:46 +0530

    ready to deploy

---
+++ blogging/public/data/blogs-card.js
@@ -0,0 +1,15 @@
+export const data = [
+    {
+        title : 'Movies',
+        about : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eius. Doloremque quibusdam adipisci voluptas aliquam, laboriosam ullam qui corrupti eaque beatae pariatur ducimus amet, dolore nobis sequi eligendi similique ipsa veniam numquam rem eius provident, facilis quo. Architecto modi quo laboriosam, unde, quos velit, soluta assumenda aspernatur ut dolor quasi!'
+    }
+    ,
+    {
+        title : 'Mobile',
+        about : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eius. Doloremque quibusdam adipisci voluptas aliquam, laboriosam ullam qui corrupti eaque beatae pariatur ducimus amet, dolore nobis sequi eligendi similique ipsa veniam numquam rem eius provident, facilis quo. Architecto modi quo laboriosam, unde, quos velit, soluta assumenda aspernatur ut dolor quasi!'
+    },
+    {
+        title : 'News',
+        about : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eius. Doloremque quibusdam adipisci voluptas aliquam, laboriosam ullam qui corrupti eaque beatae pariatur ducimus amet, dolore nobis sequi eligendi similique ipsa veniam numquam rem eius provident, facilis quo. Architecto modi quo laboriosam, unde, quos velit, soluta assumenda aspernatur ut dolor quasi!'
+    }
+]
